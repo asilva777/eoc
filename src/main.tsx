a3cdf1a07facf173d187fefc77893fa8667ada36
ASilva Innovations <106929764+asilva777@users.noreply.github.com>
2025-10-31 08:31:58 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
 import { createRoot } from "react-dom/client";
+import React from "react";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Failed to find the root element");
+}
+
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
